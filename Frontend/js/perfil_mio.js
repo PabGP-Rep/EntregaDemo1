63807f0cda153f83d5c2c15b9285a37ec57c06bc
angelsequeda <86447103+angelsequeda@users.noreply.github.com>
2021-07-26 14:46:35 -0500

    Update perfil_mio.js

--- Frontend/js/perfil_mio.js
+++ Frontend/js/perfil_mio.js
@@ -7,14 +7,12 @@ Storage.subirNuevoCliente();
 document.getElementById('boton-actualizar').addEventListener('click', async ()=>{
     try {
         let cliente = Storage.crearCliente();
-        console.log(cliente);
         let resultado = await CRUDCliente.actualizar_usuario({PAPEL:'Usuario',NOMBRE1:cliente.nombre1,NOMBRE2:cliente.nombre2,APELLIDO1:cliente.apellido1,
         APELLIDO2:cliente.apellido2,USERNAME:cliente.username,PASSWORD_USUARIO:cliente.password,DIRECCION:cliente.direccion,
         ENVIOS:cliente.envios, PAIS: cliente.pais, FORMA_PAGO: cliente.pago, PROPIETARIO_TARJETA: cliente.propietario,CADUCIDAD:cliente.caducidad, NUM_TARJETA: cliente.tarjeta, PASSWORD_USUARIO: cliente.password, MAIL: cliente.mail, TELEFONO: cliente.tel,CVV: cliente.cvv } )
-        let nuevoCliente = Storage.crearCliente();
-        console.log(nuevoCliente);
-        localStorage.setItem('usuarioActivo',JSON.stringify(nuevoCliente));
-        alert(resultado)
+        alert(resultado);
+        let buscado =await CRUDCliente.consultar_usuario({USERNAME: document.getElementById('username').value,PASSWORD_USUARIO:document.getElementById('password').value});
+        Storage.crearCliente();
         Storage.subirNuevoCliente();
     } catch (error) {
         console.log(error);
@@ -26,7 +24,7 @@ document.getElementById('boton-eliminar').addEventListener('click',async ()=>{
     try {
         let resultado = await CRUDCliente.borrar_usuario({USERNAME:document.getElementById('username').value,PASSWORD_USUARIO:document.getElementById('password').value})
         alert(resultado);
-        if(resultado === 'Cliente eliminado correctamente [CONTROLLER]') {
+        if(resultado === 'Usuario eliminado con exito') {
             Storage.borrar();
             window.open('../index.html','_self')
         }

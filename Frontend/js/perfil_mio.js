04202a742b2bca3c1c1e5be022ee14e5012a1f85
angelsequeda <86447103+angelsequeda@users.noreply.github.com>
2021-07-26 14:42:07 -0500

    Fixed

--- Frontend/js/perfil_mio.js
+++ Frontend/js/perfil_mio.js
@@ -7,13 +7,14 @@ Storage.subirNuevoCliente();
 document.getElementById('boton-actualizar').addEventListener('click', async ()=>{
     try {
         let cliente = Storage.crearCliente();
+        console.log(cliente);
         let resultado = await CRUDCliente.actualizar_usuario({PAPEL:'Usuario',NOMBRE1:cliente.nombre1,NOMBRE2:cliente.nombre2,APELLIDO1:cliente.apellido1,
         APELLIDO2:cliente.apellido2,USERNAME:cliente.username,PASSWORD_USUARIO:cliente.password,DIRECCION:cliente.direccion,
         ENVIOS:cliente.envios, PAIS: cliente.pais, FORMA_PAGO: cliente.pago, PROPIETARIO_TARJETA: cliente.propietario,CADUCIDAD:cliente.caducidad, NUM_TARJETA: cliente.tarjeta, PASSWORD_USUARIO: cliente.password, MAIL: cliente.mail, TELEFONO: cliente.tel,CVV: cliente.cvv } )
+        let nuevoCliente = Storage.crearCliente();
+        console.log(nuevoCliente);
+        localStorage.setItem('usuarioActivo',JSON.stringify(nuevoCliente));
         alert(resultado)
-        let buscado =await CRUDCliente.consultar_usuario({USERNAME: document.getElementById('username').value,PASSWORD_USUARIO:document.getElementById('password').value})
-        console.log(buscado);
-        localStorage.setItem('usuarioActivo',JSON.stringify(buscado))
         Storage.subirNuevoCliente();
     } catch (error) {
         console.log(error);

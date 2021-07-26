fb1e38250fa08a05d53429c19c7a822dce49184f
angelsequeda <86447103+angelsequeda@users.noreply.github.com>
2021-07-26 12:14:59 -0500

    Cambio usuario-contraseña

--- Backend/routes/clientes.routes.js
+++ Backend/routes/clientes.routes.js
@@ -5,11 +5,11 @@ module.exports = (app) =>{
 
   app.post('/clientes/nuevo', clienteDatosEnviados, clienteExiste, crearCliente);
 
-  app.post('/clientes/miperfil', clienteUsuarioEnviado, checarCliente, buscarCliente);
+  app.post('/clientes/miperfil', clienteUsuarioEnviado,  buscarCliente);
 
   app.post('/clientes', clienteUsuarioEnviado, checarCliente, puedeVerInfo, listarClientes);
 
-  app.post('/clientes/actualizar',clienteUsuarioEnviado,checarCliente, actualizarCliente);
+  app.post('/clientes/actualizar',clienteUsuarioEnviado, actualizarCliente);
 
   app.post('/clientes/eliminar',clienteUsuarioEnviado,checarCliente, eliminarCliente);      
       
